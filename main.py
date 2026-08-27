92fef173e5311e031faac4af7b89d755ee9b1809
michal <michalzmichalowic@gmail.com>
2026-08-27 07:30:46 +0200

    Both the moon and the planet move

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ class Body:
         self.state = state
 
 planet = Body(100, np.array([[0, 0, 0],
-                                  [0, 0, 0]]))
+                                  [0, -0.13, 0]]))
 
 moon = Body(1, np.array([[1, 0, 0],
                                [0, 13, 0]]))
 
 G = 1
 
-t = np.linspace(0, 2, 1000)
+t = np.linspace(0, 8, 1000)
 
 def acceleration(on, due):
     return (- G * due.mass * (on.state[0] - due.state[0]) /
@@ -25,24 +25,37 @@ def acceleration(on, due):
 # print(acceleration(moon, planet))
 # print(acceleration(planet, moon))
 
-def state_derivative(t, state, due):
-    r = state[:3]
-    v = state[3:]
-    r_due = due.state[0]
+system_state = np.concatenate((moon.state.flatten(), planet.state.flatten()))
 
-    a = - G * due.mass * (r - r_due) / np.linalg.norm(r - r_due) ** 3
+# print(system_state)
 
-    return np.array([v, a]).flatten()
+def state_derivative(t, state, moon, planet):
+    r_moon = state[:3]
+    v_moon = state[3:6]
+    r_planet = state[6:9]
+    v_planet = state[9:]
 
-solution_moon = rk4(t, moon.state.flatten(), state_derivative, args=(planet,))
+    a_moon = - G * planet.mass * (r_moon - r_planet) / np.linalg.norm(r_moon - r_planet) ** 3
+
+    a_planet = - G * moon.mass * (r_planet - r_moon) / np.linalg.norm(r_planet - r_moon) ** 3
+
+    return np.concatenate([v_moon, a_moon, v_planet, a_planet])
+
+solution = rk4(t, system_state, state_derivative, args=(moon, planet,))
 # solution_planet = rk4(t, planet.state.flatten(), state_derivative, args=(moon,))
 
 fig, ax = plt.subplots()
 ax.set_aspect('equal')
 
-ax.plot(solution_moon[:, 0], solution_moon[:, 1])
-ax.plot(solution_moon[0, 0], solution_moon[0, 1], marker='o', label='Moon startpoint')
-ax.plot(solution_moon[-1, 0], solution_moon[-1, 1], marker='o', label='Moon endpoint')
-# ax.plot(solution_planet[:, 0], solution_planet[:, 1])
+# Plot the trajectory of the moon
+ax.plot(solution[:, 0], solution[:, 1], color='r')
+ax.plot(solution[0, 0], solution[0, 1], marker='o', label='Moon startpoint', color='#730202')
+ax.plot(solution[-1, 0], solution[-1, 1], marker='o', label='Moon endpoint', color='#ed0202')
+
+# Plot the trajectory of the planet
+ax.plot(solution[:, 6], solution[:, 7], color='b')
+ax.plot(solution[0, 6], solution[0, 7], marker='o', label='Planet startpoint', color='#015457')
+ax.plot(solution[-1, 6], solution[-1, 7], marker='o', label='Planet endpoint', color='#00cfd6')
+
 plt.legend()
 plt.show()
