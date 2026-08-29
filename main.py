0a2b2a129cf47c1ed25c0339af3a6fcdebc7480f
michal <michalzmichalowic@gmail.com>
2026-08-29 08:49:11 +0200

    3 body problem solved!

--- main.py
+++ main.py
@@ -6,49 +6,69 @@ from physicslib.ode import *
 class Body:
     def __init__(self, mass, state):
         self.mass = mass
-        self.state = state
+        self.state = state.flatten()
 
-planet = Body(100, np.array([[0, 0, 0],
-                                  [0, -0.13, 0]]))
+sun = Body(10000, np.array([[0, 0, 0],
+                                 [0, -0.1, 0]]))
 
-moon = Body(1, np.array([[1, 0, 0],
+
+planet = Body(100, np.array([[10, 0, 0],
+                                  [0, 20-0.13, 0]]))
+
+moon = Body(1, np.array([[11, 0, 0],
                                [0, 13, 0]]))
+
+bodies_list = [sun, planet, moon]
 
 G = 1
 
 t = np.linspace(0, 8, 1000)
 
-def acceleration(on, due):
-    return (- G * due.mass * (on.state[0] - due.state[0]) /
-            np.linalg.norm(on.state[0] - due.state[0])**3)
+system_state = np.zeros((len(bodies_list), 6))
+for i, body in enumerate(bodies_list):
+    system_state[i, :] = body.state
 
-# print(acceleration(moon, planet))
-# print(acceleration(planet, moon))
 
-system_state = np.concatenate((moon.state.flatten(), planet.state.flatten()))
+def state_derivative(t, state, bodies):
+    state = state.reshape((len(bodies), 6))
 
-# print(system_state)
+    r = state[:, :-3]
+    v = state[:, 3:]
+    a = np.zeros_like(r)
 
-def state_derivative(t, state, moon, planet):
-    r_moon = state[:3]
-    v_moon = state[3:6]
-    r_planet = state[6:9]
-    v_planet = state[9:]
+    for i, body_on in enumerate(bodies):
+        acceleration = np.zeros(3)
 
-    a_moon = - G * planet.mass * (r_moon - r_planet) / np.linalg.norm(r_moon - r_planet) ** 3
+        for j, body_due in enumerate(bodies):
+            if i == j:
+                continue
 
-    a_planet = - G * moon.mass * (r_planet - r_moon) / np.linalg.norm(r_planet - r_moon) ** 3
+            acceleration += - G * body_due.mass * (r[i] - r[j]) / np.linalg.norm(r[i] - r[j]) ** 3
 
-    return np.concatenate([v_moon, a_moon, v_planet, a_planet])
+        a[i] = acceleration
 
-solution = rk4(t, system_state, state_derivative, args=(moon, planet,))
-# solution_planet = rk4(t, planet.state.flatten(), state_derivative, args=(moon,))
+    derivative = np.concatenate((v, a), 1)
+
+    return derivative.flatten()
+
+system_state = np.concatenate([body.state for body in bodies_list])
+
+solution = rk4(t, system_state, state_derivative, args=(bodies_list,))
+
+solution = solution.reshape((len(t), len(bodies_list), 6))
 
 fig, ax = plt.subplots()
 ax.set_aspect('equal')
 
+# Plot the trajectory of the sun
+ax.plot(solution[:, 0, 0], solution[:, 0, 1], color='r')
+
+# Plot the trajectory of the planet
+ax.plot(solution[:, 1, 0], solution[:, 1, 1], color='b')
+
 # Plot the trajectory of the moon
-ax.plot(solution[:, 0], solution[:, 1], color='r')
+ax.plot(solution[:, 2, 0], solution[:, 2, 1], color='g')
+'''
 ax.plot(solution[0, 0], solution[0, 1], marker='o', label='Moon startpoint', color='#730202')
 ax.plot(solution[-1, 0], solution[-1, 1], marker='o', label='Moon endpoint', color='#ed0202')
 
@@ -58,4 +78,5 @@ ax.plot(solution[0, 6], solution[0, 7], marker='o', label='Planet startpoint', c
 ax.plot(solution[-1, 6], solution[-1, 7], marker='o', label='Planet endpoint', color='#00cfd6')
 
 plt.legend()
+'''
 plt.show()
