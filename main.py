730b8869aae32cbe361b116f7e1955875b310e21
michal <michalzmichalowic@gmail.com>
2026-08-29 15:36:49 +0200

    The solar system

--- main.py
+++ main.py
@@ -8,26 +8,34 @@ class Body:
         self.mass = mass
         self.state = state.flatten()
 
-sun = Body(10000, np.array([[0, 0, 0],
-                                 [0, -0.1, 0]]))
+# The units used are:
+# a.u. for distance
+# earth years for time
+# earth masses for mass
+
+sun = Body(332950, np.array([[0, 0, 0],
+                                   [0, 0, 0]]))
 
 
-planet = Body(100, np.array([[10, 0, 0],
-                                  [0, 20-0.13, 0]]))
+earth = Body(1, np.array([[1, 0, 0],
+                                [0, 2*np.pi, 0]]))
 
-moon = Body(1, np.array([[11, 0, 0],
-                               [0, 13, 0]]))
 
-bodies_list = [sun, planet, moon]
+moon = Body(0.012, np.array([[1 + 2.57e-3, 0, 0],
+                                   [0, 2*np.pi + 0.216, 0]]))
 
-G = 1
 
-t = np.linspace(0, 8, 1000)
+mars = Body(0.107, np.array([[1.524, 0, 0],
+                                   [0, 5.09, 0]]))
 
-system_state = np.zeros((len(bodies_list), 6))
-for i, body in enumerate(bodies_list):
-    system_state[i, :] = body.state
+bodies_list = [sun, earth, moon, mars]
 
+G = 6.674 * 10 ** -11 * 5.97e24 * 1.496e11**-3 * (3600*24*365.25)**2
+print(G)
+
+t = np.linspace(0, 3, 10000)
+
+system_state = np.concatenate([body.state for body in bodies_list])
 
 def state_derivative(t, state, bodies):
     state = state.reshape((len(bodies), 6))
@@ -36,22 +44,21 @@ def state_derivative(t, state, bodies):
     v = state[:, 3:]
     a = np.zeros_like(r)
 
-    for i, body_on in enumerate(bodies):
-        acceleration = np.zeros(3)
-
+    for i in range(len(bodies)):
         for j, body_due in enumerate(bodies):
             if i == j:
                 continue
 
-            acceleration += - G * body_due.mass * (r[i] - r[j]) / np.linalg.norm(r[i] - r[j]) ** 3
+            dr = r[i] - r[j]
 
-        a[i] = acceleration
+            a[i] += (
+                    -G * body_due.mass
+                    * dr
+                    / np.linalg.norm(dr) ** 3
+            )
 
-    derivative = np.concatenate((v, a), 1)
+    return np.concatenate((v, a), axis=1).flatten()
 
-    return derivative.flatten()
-
-system_state = np.concatenate([body.state for body in bodies_list])
 
 solution = rk4(t, system_state, state_derivative, args=(bodies_list,))
 
@@ -60,20 +67,25 @@ solution = solution.reshape((len(t), len(bodies_list), 6))
 fig, ax = plt.subplots()
 ax.set_aspect('equal')
 
-# Plot the trajectory of the sun
+# Plot the trajectory of the Sun
 ax.plot(solution[:, 0, 0], solution[:, 0, 1], color='r')
+ax.plot(solution[0, 0, 0], solution[0, 0, 1], marker='o', label='Sun startpoint', color='#730202')
+ax.plot(solution[-1, 0, 0], solution[-1, 0, 1], marker='o', label='Sun endpoint', color='#ed0202')
 
-# Plot the trajectory of the planet
+# Plot the trajectory of the Earth
 ax.plot(solution[:, 1, 0], solution[:, 1, 1], color='b')
 
-# Plot the trajectory of the moon
+# Plot the trajectory of the Moon
 ax.plot(solution[:, 2, 0], solution[:, 2, 1], color='g')
+
+# Plot the trajectory of Mars
+ax.plot(solution[:, 3, 0], solution[:, 3, 1], color='r')
+
 '''
 ax.plot(solution[0, 0], solution[0, 1], marker='o', label='Moon startpoint', color='#730202')
 ax.plot(solution[-1, 0], solution[-1, 1], marker='o', label='Moon endpoint', color='#ed0202')
 
 # Plot the trajectory of the planet
-ax.plot(solution[:, 6], solution[:, 7], color='b')
 ax.plot(solution[0, 6], solution[0, 7], marker='o', label='Planet startpoint', color='#015457')
 ax.plot(solution[-1, 6], solution[-1, 7], marker='o', label='Planet endpoint', color='#00cfd6')
 
