da2396f732c301728345d493fafc19330c038d37
michal <michalzmichalowic@gmail.com>
2026-08-30 10:02:16 +0200

    Add toggle view button

--- gui.py
+++ gui.py
@@ -19,6 +19,9 @@ class MainWindow(QMainWindow):
         self.sim = simulation
         self.sol = solution
         self.frame = 0
+        self.first_draw = True
+
+        self.view_mode = "solar_system"
 
         self.create_plot()
         self.create_controls()
@@ -45,30 +48,56 @@ class MainWindow(QMainWindow):
             self.frame_changed
         )
 
+        self.view_button = QPushButton("Earth–Moon view")
+        self.view_button.clicked.connect(self.toggle_view)
+
     def create_layout(self):
         central_widget = QWidget()
 
         layout = QVBoxLayout()
+        controls_layout = QHBoxLayout()
 
         layout.addWidget(self.toolbar)
         layout.addWidget(self.canvas)
-        layout.addWidget(self.time_slider)
+
+        controls_layout.addWidget(self.time_slider)
+        controls_layout.addWidget(self.view_button)
+        layout.addLayout(controls_layout)
 
         central_widget.setLayout(layout)
 
         self.setCentralWidget(central_widget)
 
-    def update_plot(self):
+    def update_plot(self, keep_zoom=True):
+        if keep_zoom and not self.first_draw and self.view_mode == "solar_system":
+            xlim = self.ax.get_xlim()
+            ylim = self.ax.get_ylim()
+
         self.ax.clear()
 
-        draw_scene(self.ax, self.sol, self.frame)
+        draw_scene(self.ax, self.sol, self.frame, self.view_mode)
+
+        if keep_zoom and not self.first_draw and self.view_mode == "solar_system":
+            self.ax.set_xlim(xlim)
+            self.ax.set_ylim(ylim)
 
         self.ax.set_aspect('equal')
         self.canvas.draw()
+        self.first_draw = False
 
     def frame_changed(self):
         self.frame = self.time_slider.value()
         self.update_plot()
+
+    def toggle_view(self):
+        if self.view_mode == "solar_system":
+            self.view_mode = "earth_moon"
+            self.view_button.setText("Solar system view")
+        else:
+            self.view_mode = "solar_system"
+            self.view_button.setText("Earth Moon view")
+
+        self.update_plot(keep_zoom=False)
 
 def draw_window(simulation, solution):
     app = QApplication(sys.argv)
--- plotting.py
+++ plotting.py
@@ -1,27 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def draw_scene(ax, solution, frame):
-
-    '''
-    # Plot the trajectory of the Sun
-    ax.plot(solution[:, 0, 0], solution[:, 0, 1], color='r')
-    ax.plot(solution[0, 0, 0], solution[0, 0, 1], marker='o', label='Sun startpoint', color='#730202')
-    ax.plot(solution[-1, 0, 0], solution[-1, 0, 1], marker='o', label='Sun endpoint', color='#ed0202')
-
-    # Plot the trajectory of the Earth
-    ax.plot(solution[:, 1, 0], solution[:, 1, 1], color='b')
-
-    # Plot the trajectory of the Moon
-    ax.plot(solution[:, 2, 0], solution[:, 2, 1], color='g')
-
-    # Plot the trajectory of Mars
-    ax.plot(solution[:, 3, 0], solution[:, 3, 1], color='r')
-    '''
-
+def draw_scene(ax, solution, frame, view_mode):
     for i in range(solution.shape[1]):
         # Plot the trajectory
-        ax.plot(solution[:frame+1, i, 0], solution[:frame+1, i, 1])
+        ax.plot(solution[:frame + 1, i, 0], solution[:frame + 1, i, 1])
 
         # Plot the position at a particular frame
-        ax.plot(solution[frame, i, 0], solution[frame, i, 1], marker='o', color='#730202')
+        ax.plot(solution[frame, i, 0], solution[frame, i, 1], marker='o')
+
+    if view_mode == "solar_system":
+        ax.set_xlim(-31, 31)
+        ax.set_ylim(-31, 31)
+
+    elif view_mode == "earth_moon":
+        earth_x = solution[frame, 3, 0]
+        earth_y = solution[frame, 3, 1]
+        dist = 0.004
+
+        ax.set_xlim(earth_x - dist, earth_x + dist)
+        ax.set_ylim(earth_y - dist, earth_y + dist)
+
+    '''
+    if view_mode == "solar_system":
+        for i in range(solution.shape[1]):
+            # Plot the trajectory
+            ax.plot(solution[:frame + 1, i, 0], solution[:frame + 1, i, 1])
+
+            # Plot the position at a particular frame
+            ax.plot(solution[frame, i, 0], solution[frame, i, 1], marker='o')
+
+    elif view_mode == "earth_moon":
+        earth_position = solution[:, 3, :2]
+        moon_position = solution[:, 4, :2]
+
+        moon_relative = moon_position - earth_position
+
+        ax.plot(
+            moon_relative[:frame + 1, 0],
+            moon_relative[:frame + 1, 1]
+        )
+
+        ax.plot(
+            moon_relative[frame, 0],
+            moon_relative[frame, 1],
+            marker='o'
+        )
+
+        ax.plot(0, 0, marker='o')
+    '''
